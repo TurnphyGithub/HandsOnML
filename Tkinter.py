dc78ddc89d48a6a0c929785b97dedb58ef640c59
TurnphyGithub <48395302+TurnphyGithub@users.noreply.github.com>
2020-09-19 00:38:01 -0500

    Add files via upload
    The standard module of tkinter.

---
+++ Tkinter.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 18 23:41:41 2018
+ML with interface
+@author: Turnphy
+"""
+
+import tkinter as tk
+
+class WordRecg(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Hello World\n(click me)"
+        self.hi_there["command"] = self.say_hi
+        self.hi_there.pack(side="top")
+
+        self.quit = tk.Button(self, text="QUIT", fg="red",
+                              command=self.master.destroy)
+        self.quit.pack(side="bottom")
+    
+    def load_data(self):
+        return
+
+    def say_hi(self):
+        print("hi there, everyone!")
+
+root = tk.Tk()
+app = WordRecg(master=root)
+app.mainloop()
